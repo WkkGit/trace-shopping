eb29061d60e3edfef4d05eaab43e32fabb315646
WkkGit <17346716685@163.com>
2021-03-19 16:43:08 +0800

    init

---
+++ src/main/java/com/wkk/traceshoping/TraceShopingApplication.java
@@ -0,0 +1,13 @@
+package com.wkk.traceshoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceShopingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraceShopingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wkk/traceshoping/TraceShopingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wkk.traceshoping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceShopingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
